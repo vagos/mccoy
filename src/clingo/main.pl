f23f5397f11dcea7cf51ca3e4697c2be058fb5dd
Evangelos Labrou <e.lamprou@upnet.gr>
2023-05-30 23:23:18 +0300

    Move symptom link in main

--- src/clingo/main.pl
+++ src/clingo/main.pl
@@ -4,11 +4,11 @@
 
 has(symptom(S)) :- add(symptom(S)).
 
+has(symptom(S2)) :- has(symptom(S1)),
+                    linked_symptom(S1, S2).
+
 :- not diagnosis(_).
 
 #minimize { 1, S : has(S) }.
 
 %!show_trace diagnosis(D).
-
-#show diagnosis/1.
-#show has/1.
